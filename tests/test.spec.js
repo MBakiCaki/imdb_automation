ff4c188a3ff4d3e4aab12c44c095cd00e2221a80
MBakiCaki <muhammedbakicaki@gmail.com>
2023-03-29 16:29:25 +0300

    searchbar

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,7 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('case', async ({ page }) => {
-
+    
+    // -- FROM OSCAR AWARDS
     // navigate to the website
     await page.goto('https://www.imdb.com/');
 
@@ -21,12 +22,28 @@ test('case', async ({ page }) => {
     // assign movie info to variables
     // -hasText does not uses strict matching
     let director = await page.getByRole('listitem').filter(page.getByTestId('title-pc-principal-credit')).filter({ hasText: 'Director',}).first().innerText();//filter({ has: page.locator('span[]') })
-    // console.log(director)
-    let writer = await page.getByRole('listitem').filter(page.getByTestId('title-pc-principal-credit')).filter({ hasText: 'Writer'}).first().innerText();
-    // console.log(writer);
-    let star = await page.getByRole('listitem').filter(page.getByTestId('title-pc-principal-credit')).filter({ hasText: 'Star'}).first().innerText();
-    // console.log(star);
-    
-    // await page.pause()
 
+    let writer = await page.getByRole('listitem').filter(page.getByTestId('title-pc-principal-credit')).filter({ hasText: 'Writer'}).first().innerText();
+
+    let star = await page.getByRole('listitem').filter(page.getByTestId('title-pc-principal-credit')).filter({ hasText: 'Star'}).first().innerText();
+
+    
+    // -- FROM SEARCH
+    // navigate to main page
+    await page.getByRole('link', { name: 'Home' }).click();
+
+    // search and navigate to "The Jazz Singer" movie page
+    await page.getByPlaceholder('Search IMDb').click();
+    await page.getByPlaceholder('Search IMDb').type('The Jazz Singer');
+    await page.getByRole('link', { name: 'The Jazz Singer The Jazz Singer 1927 Al Jolson, May McAvoy' }).click();
+   
+    // find and compare movie info
+    let director2 = await page.getByRole('listitem').filter(page.getByTestId('title-pc-principal-credit')).filter({ hasText: 'Director'}).first().innerText();
+    expect(director2).toEqual(director);
+
+    let writer2 = await page.getByRole('listitem').filter(page.getByTestId('title-pc-principal-credit')).filter({ hasText: 'Writer'}).first().innerText();
+    expect(writer2).toEqual(writer);
+
+    let star2 = await page.getByRole('listitem').filter(page.getByTestId('title-pc-principal-credit')).filter({ hasText: 'Star'}).first().innerText();
+    expect(star2).toEqual(star);
 })
