f45d4c63d3393e88b193a6071d3b894bc1d0c012
MBakiCaki <muhammedbakicaki@gmail.com>
2023-03-29 15:11:31 +0300

    oscar

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.only('case', async ({ page }) => {
+test('case', async ({ page }) => {
 
     // navigate to the website
     await page.goto('https://www.imdb.com/');
@@ -18,8 +18,15 @@ test.only('case', async ({ page }) => {
 
     await honoraryAwards.locator('.event-widgets__nominee-name', {hasText: 'The Jazz Singer'}).click()
     
-    await page.pause()
+    // assign movie info to variables
+    // -hasText does not uses strict matching
+    let director = await page.getByRole('listitem').filter(page.getByTestId('title-pc-principal-credit')).filter({ hasText: 'Director',}).first().innerText();//filter({ has: page.locator('span[]') })
+    // console.log(director)
+    let writer = await page.getByRole('listitem').filter(page.getByTestId('title-pc-principal-credit')).filter({ hasText: 'Writer'}).first().innerText();
+    // console.log(writer);
+    let star = await page.getByRole('listitem').filter(page.getByTestId('title-pc-principal-credit')).filter({ hasText: 'Star'}).first().innerText();
+    // console.log(star);
+    
+    // await page.pause()
 
-    // await page.getByTestId('plot').click();
-    await page.pause()
 })
